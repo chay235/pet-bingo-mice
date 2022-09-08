8945a2b559fa90d80621bc24d907f60f22953bcc
chay235 <008133327@coyote.csusb.edu>
2022-09-07 22:51:40 -0700

    Initial version of code
    Draft version of code

---
+++ Assets/ReturnMain.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        // Debug.log("QUIT!");
+        Application.Quit();
+    }
+}
