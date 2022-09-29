379b871e3cc160629d882c5c3c37f73b1d8ed109
N1th33sha <112601414+N1th33sha@users.noreply.github.com>
2022-09-29 15:08:04 -0700

    Petbingo

---
+++ Assets/Auto_Move.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Auto_Move : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+}
